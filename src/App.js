fb8663505a9112d0c38ef305c1916c684bccdfb7
Ezra Marcia <59544953+exra2mp@users.noreply.github.com>
2024-06-09 13:52:50 -0600

    Add files via upload
    React Project with functional component

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
+import ListaTareas from './componentes/ListaTareas';
+
+function App() {
+  return (
+    <div className="aplicacion-tareas">
+      <div className= 'freecodecamp-logo-contenedor'>
+        <img 
+        src={freeCodeCampLogo} 
+        className='freecodecamp-logo' />
+      </div>
+      <div className='tareas-lista-principal'>
+        <h1>Mis Tareas</h1>
+        <ListaTareas />
+      </div>
+    </div>
+  );
+}
+
+export default App;
